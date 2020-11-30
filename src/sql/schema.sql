f53dbefe1f5242e0d9192b0b5e3176644cf88ab3
Rivaldo1123 <ramanlewis@gmail.com>
2020-11-30 17:04:36 -0500

    Updated Database

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,59 +1,42 @@
-/*  phpMyAdmin SQL Dump
- version 5.0.4
- https://www.phpmyadmin.net/
-
- Host: 127.0.0.1
- Generation Time: 
- Server version: 10.4.16-MariaDB
- PHP Version: 7.4.12 
-
- */
-
--- Database: BugMe;
 
 CREATE DATABASE IF NOT EXISTS BugMe;
+USE BugMe;
+-- Database: BugMe;
 
 -- Table structure for UserTable;
 
 CREATE TABLE IF NOT EXISTS UserTable (
-    id INT,
+    id INT AUTO_INCREMENT,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
-    password_hash VARCHAR(64) NOT NULL,
-    email VARCHAR(200),
-    date_joined DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    password VARCHAR(64) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    date_joined DATETIME,
     PRIMARY KEY (id)
-) ENGINE = MyISAM AUTO_INCREMENT = 4080 DEFAULT CHARSET = utf8mb4;
+) ENGINE = MyISAM AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS  IssuesTable(
-    id INT AUTO_INCREMENT,
-    title VARCHAR(64),
-    description TEXT(100),
-    type VARCHAR(64),
-    priority VARCHAR(64),
-    status VARCHAR(64),
-    assigned_to INT,
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(64) NOT NULL,
+    description TEXT(500) NOT NULL,
+    type VARCHAR(64) NOT NULL,
+    priority VARCHAR(20) NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    assigned_to INT NULL,
     created_by INT,
     created DATETIME,
     updated DATETIME,
     PRIMARY KEY (id)
-);
+)ENGINE = MyISAM DEFAULT CHARSET = utf8mb4;
 
 /*
  Data for table UserTable
 For markers password is password123
  */
-INSERT INTO UserTable (id,firstname,lastname,password_hash,email,date_joined) VALUES 
-('1', 'Raman', 'Lewis', '', 'admin@project2.com', '2020-11-24 22:16:00'),
-('2', 'Candice', 'Giselle', '', 'admin@project2.com', '2020-11-20 14:30:00'),
-('3', 'Dexter', 'Small', '', 'admin@project2.com', '2020-11-18 15:47:00'),
-('4', 'Samara', 'Soares', '',  'admin@project2.com', '2020-11-16 11:38:00');
-
-INSERT INTO IssuesTable (title,description,type,status,assigned_to,created) VALUES 
-('#100','Add User Form','Bug','Open','1','2020-11-25 22:16:00');
+INSERT INTO UserTable (firstname,lastname,password,email,date_joined) VALUES 
+('Admin', 'User', 'password123', 'admin@project2.com', NOW());
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 ;
