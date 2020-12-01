b083661228a2cab4c4a4c6b0fc13134ab0fe5ed2
Rivaldo1123 <ramanlewis@gmail.com>
2020-11-30 19:53:20 -0500

    Incomplete JS File

--- src/js/main.js
+++ src/js/main.js
@@ -1,67 +1,76 @@
 'user strict'
 
- 
+
 window.onload = function() {
-	main();
+    main();
 }
 
 
 // Main function
 function main() {
-
+    addUserFormValidation(first_name, last_name, password, email);
 }
 
 
 // As a note for writing if statements,
 // check for incorrect values first 
 function loginFormValidation(email, password) {
-	// checks to see if values are correct then return true or false if the incorrect
-	// eg. 
-	var error = false;
+    // checks to see if values are correct then return true or false if the incorrect
+    // eg. 
+    var error = false;
+    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
 
-	if ( nullOrBlank(email) || !email.match("add regex here for email")) {
-		error = true; 
-	}
+    if (nullOrBlank(email) || !email.match(emailRegex)) {
+        error = true;
+    }
 
-
-	.. if (password) .. etc. 
+    if (password);
 }
 
 function addUserFormValidation(first_name, last_name, password, email) {
-	// checks to see if values are correct then return true or false if the incorrect
+    // checks to see if values are correct then return true or false if the incorrect
+    var first_name = document.getElementById('first_name').value;
+    var last_name = document.getElementById('last_name').value;
+    var password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+
+    if (first_name == '' || last_name == '' || password == '' || email == '') {
+        return false
+    }
+    return true
 }
 
 function newIssueFormValidation() {
-	// checks to see if values are correct then return true or false if the incorrect
+    // checks to see if values are correct then return true or false if the incorrect
 }
- 
+
 
 /**
  * Checks to see if a string is null or blank
  * @param {string} from link to fetch data  
  * @param {element} set_to to set the returned data to
  * @return return function if the wrong status code is recieved
-*/
+ */
 function fetchData(event, from, set_to) {
-	event.preventDefault();
+    event.preventDefault();
 
-	fetch(from) 
-		.then(function(response) {
-			if (response.status !== 200) {
-				console.error(`Something went wrong. Status Code: ${response.status}`);
-				// todo : write code to show user an error message, like a popup
-				return;
-			}
+    fetch(from)
+        .then(function(response) {
+            if (response.status !== 200) {
+                console.error(`Something went wrong. Status Code: ${response.status}`);
+                // todo : write code to show user an error message, like a popup
+                return;
+            }
 
-			// Get data and update ui
-			response.text().then(function(promise) {
-				updateUI(set_to, promise);
-			});
-		})
-		.catch(function(error) {
-			console.error(`Fetching error: ${error}`);
-		});
-	
+            // Get data and update ui
+            response.text().then(function(promise) {
+                updateUI(set_to, promise);
+            });
+        })
+        .catch(function(error) {
+            console.error(`Fetching error: ${error}`);
+        });
+
 }
 
 
@@ -69,9 +78,9 @@ function fetchData(event, from, set_to) {
  * Sets an element innerHTML
  * @param {element} element for content to be set
  * @param {string} promise string to be sent to element
-*/
+ */
 function updateUI(element, promise) {
-	element.innerHTML = promise;
+    element.innerHTML = promise;
 }
 
 
@@ -83,7 +92,7 @@ function updateUI(element, promise) {
  * Checks to see if a string is null or blank
  * @param {string} word to check.
  * @return true or false
-*/
+ */
 function nullOrBlank(arg) {
-	return (arg == "" || arg == null); 
+    return (arg == "" || arg == null);
 }
